5edc590f6b669ea734de9b0b715cb3765346b113
nickuchida <nicholas.y.uchida@gmail.com>
2019-12-11 01:49:24 +0000

    adding space after last element

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -3,4 +3,4 @@ for i in range(1, 101):
     if i != 100:
         print('Fizz' * (not i % 3) + 'Buzz' * (not i % 5) or str(i), end=" ")
     else:
-        print('Fizz' * (not i % 3) + 'Buzz' * (not i % 5) or str(i))
+        print('Fizz ' * (not i % 3) + 'Buzz ' * (not i % 5) or str(i))

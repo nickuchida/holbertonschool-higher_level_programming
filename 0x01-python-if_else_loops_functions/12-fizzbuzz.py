134ed48c9636837b6ec6785b61c64e81839e0bdf
nickuchida <nicholas.y.uchida@gmail.com>
2019-12-11 01:47:55 +0000

    adding new line to fizzbuzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 for i in range(1, 101):
-    print('Fizz' * (not i % 3) + 'Buzz' * (not i % 5) or str(i), end=" ")
+    if i != 100:
+        print('Fizz' * (not i % 3) + 'Buzz' * (not i % 5) or str(i), end=" ")
+    else:
+        print('Fizz' * (not i % 3) + 'Buzz' * (not i % 5) or str(i))

135de9f2687cb2f63386cff1e679665a0bdb8384
nickuchida <nicholas.y.uchida@gmail.com>
2019-12-11 02:48:41 +0000

    returning to basic fizzbuzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
-for i in range(1, 101):
-    print('Fizz' * (not i % 3) + 'Buzz' * (not i % 5) or str(i), end=" ")
-print('')
+def fizzbuzz():
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz", end="")
+        elif i % 3 == 0:
+            print("Fizz", end="")
+        elif i % 5 == 0:
+            print("Buzz", end="")
+        else:
+            print("{:d}".format(i), end="")
+        print("", end=" ")

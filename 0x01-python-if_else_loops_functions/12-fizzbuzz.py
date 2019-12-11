66c7b0df2e7979ace656c68eaffa4053bd72c773
nickuchida <nicholas.y.uchida@gmail.com>
2019-12-11 02:36:56 +0000

    trying new line again

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 for i in range(1, 101):
-    if i != 100:
-        print('Fizz' * (not i % 3) + 'Buzz' * (not i % 5) or str(i), end=" ")
-    else:
-        print('Fizz ' * (not i % 3) + 'Buzz ' * (not i % 5) or str(i))
+    print('Fizz' * (not i % 3) + 'Buzz' * (not i % 5) or str(i), end=" ")
+print('')

6bd6252ddb93ddc6dc04f844a89d26fb5cf31350
nickuchida <nicholas.y.uchida@gmail.com>
2019-12-18 02:44:39 +0000

    removing main file for testing

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,13 +2,3 @@
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
         print(' '.join('{:d}'.format(num) for num in i))
-
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-
-print_matrix_integer(matrix)
-print("--")
-print_matrix_integer()

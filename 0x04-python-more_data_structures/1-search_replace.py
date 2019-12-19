eefde8d728dd7303a0e92081aa534e47baef4602
nickuchida <nicholas.y.uchida@gmail.com>
2019-12-19 21:55:07 +0000

    returning a new list instead of modified list

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     for i, j in enumerate(my_list):
+        new_list = my_list
         if j == search:
-            my_list[i] = replace
-    return my_list
+            new_list[i] = replace
+    return new_list

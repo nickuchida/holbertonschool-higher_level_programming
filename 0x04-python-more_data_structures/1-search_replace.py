e9b798605e1be5043e55308c4c0d887e272cc60c
nickuchida <nicholas.y.uchida@gmail.com>
2019-12-19 23:40:29 +0000

    making a copy of original list so that it doesn't get changed

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list = my_list
+    new_list = my_list[:]
     for i, j in enumerate(new_list):
         if j == search:
             new_list[i] = replace

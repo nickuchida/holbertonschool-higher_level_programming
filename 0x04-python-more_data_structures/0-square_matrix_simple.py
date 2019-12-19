0e36abfb1efa86a19fc063bdf784612bf284b8cf
nickuchida <nicholas.y.uchida@gmail.com>
2019-12-19 23:26:43 +0000

    putting map inside of a loop, appending results to each new matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    newmatrix = map(lambda x: x ** 2, matrix)
+    newmatrix = []
+    for i in matrix:
+        new = list(map(lambda x: x ** 2, i))
+        newmatrix.append(new)
     return newmatrix

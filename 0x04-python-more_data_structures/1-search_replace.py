794ddf369a427ac42c17f6e00865be6b31beb3da
nickuchida <nicholas.y.uchida@gmail.com>
2019-12-19 22:28:22 +0000

    moving where new list is set equal to mylist

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    for i, j in enumerate(my_list):
-        new_list = my_list
+    new_list = my_list
+    for i, j in enumerate(new_list):
         if j == search:
             new_list[i] = replace
     return new_list

45f46d574f70a292bd500effb7b6900cb6015c0f
nickuchida <nicholas.y.uchida@gmail.com>
2020-01-09 02:38:20 +0000

    task 5 raise

---
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def raise_exception():
+    raise TypeError
+
+raise_exception = __import__('5-raise_exception').raise_exception
+
+try:
+    raise_exception()
+except TypeError as te:
+    print("Exception raised")

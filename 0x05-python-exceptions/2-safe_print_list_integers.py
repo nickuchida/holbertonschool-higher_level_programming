077d2fb519b59da2d7bfed19c5ab1da77096ede5
nickuchida <nicholas.y.uchida@gmail.com>
2020-01-09 02:48:24 +0000

    adding another error to except

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -5,7 +5,7 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             element += 1
             print('{:d}'.format(my_list[i]), end='')
-        except(ValueError):
+        except(TypeError, ValueError):
             continue
     print()
     return element

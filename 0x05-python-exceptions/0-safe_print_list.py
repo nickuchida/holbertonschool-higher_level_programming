a8b503f4ebf8cb82ebe9473732e6d543e6886ca6
nickuchida <nicholas.y.uchida@gmail.com>
2020-01-09 01:59:39 +0000

    task 0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    element = 0;
+    for i in range(x):
+        try:
+            ++element
+            print(my_list[i], end='')
+        except:
+            break
+    print()
+    return element

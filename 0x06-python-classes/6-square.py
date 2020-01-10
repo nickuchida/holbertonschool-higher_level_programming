98d66eccf42a0b28abb9752a37ac099cfc8c1223
nickuchida <nicholas.y.uchida@gmail.com>
2020-01-10 01:12:40 +0000

    printing space instead of empty print

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -38,7 +38,7 @@ class Square():
             print()
         else:
             for s in range(self.__position[1]):
-                print()
+                print('')
             for i in range(self.__size):
                 for p in range(self.__position[0]):
                     print(' ', end='')

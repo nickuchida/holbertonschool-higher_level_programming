3253bdeda37667cf2554124862cfc5d08397affa
nickuchida <nicholas.y.uchida@gmail.com>
2020-01-14 23:58:43 +0000

    task 1, set equal to value in setter

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -24,6 +24,7 @@ class Rectangle:
             raise TypeError('width must be an integer')
         if width < 0:
             raise ValueError('width must be >= 0')
+        self.__width = value
 
     @property
     def height(self):
@@ -36,3 +37,4 @@ class Rectangle:
             raise TypeError('height must be an integer')
         if height < 0:
             raise ValueError('height must be >= 0')
+        self.__height = value

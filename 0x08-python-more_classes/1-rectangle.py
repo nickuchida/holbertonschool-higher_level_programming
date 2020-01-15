a3a22c72beab0daabfba51386eb1c982ddaac84a
nickuchida <nicholas.y.uchida@gmail.com>
2020-01-15 00:06:03 +0000

    changing word heigh/width to value in setter

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -22,7 +22,7 @@ class Rectangle:
         'setter'
         if type(value) is not int:
             raise TypeError('width must be an integer')
-        if width < 0:
+        if value < 0:
             raise ValueError('width must be >= 0')
         self.__width = value
 
@@ -35,6 +35,6 @@ class Rectangle:
         '''setter'''
         if type(value) is not int:
             raise TypeError('height must be an integer')
-        if height < 0:
+        if value < 0:
             raise ValueError('height must be >= 0')
         self.__height = value

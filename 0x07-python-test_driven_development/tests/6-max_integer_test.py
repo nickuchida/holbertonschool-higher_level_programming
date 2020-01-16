09f9d0f4341902e7910d06f2260e79431df42396
nickuchida <nicholas.y.uchida@gmail.com>
2020-01-16 22:42:19 +0000

    case for type

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -10,6 +10,7 @@ class TestMaxInteger(unittest.TestCase):
     def test_none(self):
         self.assertEqual(max_integer([None]), None)
         self.assertEqual(max_integer([]), None)
-
+    def test_type(self):
+        self.assertIsInstance(max_integer([1, 2, 3]), int)
 if __name__ == '__main__':
     unittest.main()

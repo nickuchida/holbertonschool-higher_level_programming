ca1657a578dc4b44e58031bd0ef5a311b0192fb4
nickuchida <nicholas.y.uchida@gmail.com>
2020-01-21 20:40:21 +0000

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''
+6 improve geometry
+'''
+
+
+class BaseGeometry:
+    def area(self):
+        ''' public instance area '''
+        raise Exception('area() is not implemented')

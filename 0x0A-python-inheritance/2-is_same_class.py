def37f7059c34a160988e6df0fba1df30f469fec
nickuchida <nicholas.y.uchida@gmail.com>
2020-01-21 21:56:57 +0000

    moving everything to one return statement

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -5,6 +5,4 @@
 
 def is_same_class(obj, a_class):
     ''' returns True if object is specified class, false if not '''
-    if isinstance(obj, a_class):
-        return True
-    return False
+    return isinstance(obj, a_class)

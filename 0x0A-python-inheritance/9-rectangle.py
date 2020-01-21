972762760bae14dffdb9c68d58faa29fab31e8ff
nickuchida <nicholas.y.uchida@gmail.com>
2020-01-21 21:42:12 +0000

    task 9 and 10

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''
+module 9-rectangle.py
+'''
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    def __init__(self, width, height):
+        '''initiates class Rectangle'''
+        self.integer_validator('width', width)
+        self.integer_validator('height', height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        ''' public instance area'''
+        return self.__width * self.__height
+
+    def __str__(self):
+        '''prints rectangle'''
+        return '[Rectangle] {}/{}'.format(self.__width, self.__height)

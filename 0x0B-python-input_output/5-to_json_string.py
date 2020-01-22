6bc16ba4af755e01651383c9ab903af3c353a5fc
nickuchida <nicholas.y.uchida@gmail.com>
2020-01-22 02:21:56 +0000

    task 5 and 6

---
+++ 0x0B-python-input_output/5-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''
+module 5-to_json_string.py
+'''
+import json
+
+
+def to_json_string(my_obj):
+    '''the JSON representation of an object (string)'''
+    return json.dumps(my_obj)

4f14dfe1b4fe11561e246289fa2cf7ca2786296f
nickuchida <nicholas.y.uchida@gmail.com>
2020-01-28 02:20:04 +0000

    adding json to base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,7 @@
 '''
 module base
 '''
+import json
 
 
 class Base:
@@ -15,3 +16,9 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        '''return json string representation'''
+        if list_dictionaries is None:
+            return "[]"
+        return json.dumps(list_dictionaries)

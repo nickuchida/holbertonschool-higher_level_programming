3cc723ef6a252ab475fc0956e54565de00f75ff0
nickuchida <nicholas.y.uchida@gmail.com>
2020-01-28 02:15:09 +0000

    kwargs and args in square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -37,3 +37,24 @@ class Square(Rectangle):
         dicrep['x'] = self.x
         dicrep['y'] = self.y
         return dicrep
+
+    def update(self, *args, **kwargs):
+        '''updates class square'''
+        argnum = len(args)
+        if argnum >= 1:
+            self.id = args[0]
+        if argnum >= 2:
+            self.size = args[1]
+        if argnum >= 3:
+            self.x = args[2]
+        if argnum >= 4:
+            self.y = args[3]
+        if argnum == 0:
+            if 'id' in kwargs:
+                self.id = kwargs['id']
+            if 'size' in kwargs:
+                self.size = kwargs['size']
+            if 'x' in kwargs:
+                self.x = kwargs['x']
+            if 'y' in kwargs:
+                self.y = kwargs['y']
